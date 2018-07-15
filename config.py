39010c20b9e6a824159dd7ccbb9b4e975ed804ee
Gregory Sullivan <dgregs@uab.edu>
2018-07-15 17:35:34 -0500

    Renamed project folder

--- config.py
+++ config.py
@@ -6,12 +6,6 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Enable protection against *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED = True
 
-# Enable debug mode
-DEBUG = True
-
-# Set the environment to dev (not production)
-ENV = 'development'
-
 # Set this to suppress a console warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
