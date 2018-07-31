428953e42606d23b4d8ef0d81c48022ab86c6fcb
Gregory Sullivan <dgregs@uab.edu>
2018-07-31 17:20:43 -0500

    Added refresh period for weather forecasting

--- WeatherToRide/views/__init__.py
+++ WeatherToRide/views/__init__.py
@@ -1,8 +1,15 @@
 
 from .. import app, forms, models
 
+from ..utils import weather
+
 from flask import render_template
 from flask_login import current_user, login_required
+
+import datetime
+
+# How old can forecasts be before needing to be refreshed?
+max_forecast_age = datetime.timedelta(seconds=900)
 
 @app.route('/')
 def index():
@@ -21,6 +28,14 @@ def dashboard():
 
     # Get this user's locations
     locations = models.Location.query.filter_by(user_id=current_user.id)
+
+    # Update the forecasting information if needed
+    now = datetime.datetime.now()
+
+    for location in locations:
+        forecast_age = now - location.forecast.updated
+        if forecast_age > max_forecast_age:
+            weather.update_forecast(location)
 
     # Get this user's routes
     routes = models.Route.query.filter_by(user_id=current_user.id)
